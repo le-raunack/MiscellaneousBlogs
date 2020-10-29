fd6378b4301109a643ce8a0d6f3d5aa5b85deb2d
37471927 <le-raunack@users.noreply.github.com>
2020-10-29 19:46:55 +0530

    Addednavigation bar and created and Blog Listing Page and an About page

--- src/components/header.js
+++ src/components/header.js
@@ -3,17 +3,30 @@ import PropTypes from "prop-types"
 import React from "react"
 import "./layout.css"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-          {siteTitle}
+const Header = ({ siteTitle }) => {
+  const navStyle = {
+    color: "white",
+    textDecoration: "none",
+    padding: "5px 0",
+  }
+  return (
+    <header>
+      <div className="nav">
+        <h1 style={navStyle} className="home">
+          <Link to="/" style={{ color: "inherit", textDecoration: "inherit" }}>
+            {siteTitle}
+          </Link>
+        </h1>
+        <Link to="/blogs" style={navStyle} className="nav-links">
+          Blogs
         </Link>
-      </h1>
-    </div>
-  </header>
-)
+        <Link to="/about" style={navStyle} className="nav-links">
+          About
+        </Link>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
---
+++ src/pages/blogs.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogsPge = () => (
+  <Layout>
+    <SEO title="All Blogs" />
+    <h1>All Blogs on the Site</h1>
+  </Layout>
+)
+
+export default BlogsPge
